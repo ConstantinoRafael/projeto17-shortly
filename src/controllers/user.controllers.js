23ad3d73d86f693856d302e36e879ccdb98cd967
ConstantinoRafael <rafael.constantino89@gmail.com>
2022-12-17 02:06:21 -0300

    feat: create controller user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,15 +2,18 @@ import { connectionDB } from "../database/db.js";
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
 
-export async function signUp (req, res) {
-    const user = res.locals.user;
-    const passwordHash = bcrypt.hashSync(user.password, 10);
+export async function signUp(req, res) {
+  const { name, email, password } = res.locals.user;
+  const passwordHash = bcrypt.hashSync(password, 10);
 
-    try {
-        await connectionDB.query(
-        "INSERT INTO users "
-        )
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
+  try {
+    await connectionDB.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
+      [name, email, passwordHash]
+    );
+
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
